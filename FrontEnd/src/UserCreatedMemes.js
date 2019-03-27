0ac827fd38d817d7cd764b49e9ddb696750dc4f2
Zach Dickens <dickens.52@buckeyemail.osu.edu>
2019-03-27 15:12:50 -0400

    added CSS file

--- FrontEnd/src/UserCreatedMemes.js
+++ FrontEnd/src/UserCreatedMemes.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import MemeInputPanel from "./MemeInputPanel";
+import "./ThumbnailPicker.css";
 
 class UserCreatedMemes extends Component {
   render() {
     const { allMemes } = this.props;
     const userMemes = allMemes.map(item => (
       <li key={item}>
-        <img src={item} alt="meme" />
+        <p id="userTopMeme">{item.currentTopText}</p>
+        <img src={item.currentImage} alt="meme" />
+        <p id="userBottomMeme">{item.currentBottomText}</p>
       </li>
     ));
     return <ul id="userMemes">{userMemes}</ul>;
